1e300859568057af0fae30bcec89346e147ee387
zvonimir.mesaric <zvonimir.mesaric@bozic.io>
2023-11-22 17:24:43 +0100

    update twitter contract with a  struct, update existing code to fit the new logic

--- Exercises/Twitter.sol
+++ Exercises/Twitter.sol
@@ -3,17 +3,31 @@ pragma solidity ^0.8.18;
 
 contract Twitter {
 
-    mapping(address => string[]) public tweets;
-
-    function addTweet(string memory _tweet) public {
-        tweets[msg.sender].push(_tweet);
+    struct Tweet {
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getTweet(address _owner, uint _index) public view returns(string memory) {
+    mapping(address => Tweet[]) public tweets;
+
+    function addTweet(string memory _tweet) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address _owner, uint _index) public view returns(Tweet memory) {
         return tweets[_owner][_index];
     }
 
-    function getTweets(address _owner) public view returns(string[] memory) {
+    function getTweets(address _owner) public view returns(Tweet[] memory) {
         return tweets[_owner];
     }
 }
