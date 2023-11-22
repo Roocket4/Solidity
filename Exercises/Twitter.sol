1a03c72381f47de5ea2ee734e20b0ba4c88d6bdc
zvonimir.mesaric <zvonimir.mesaric@bozic.io>
2023-11-22 16:48:08 +0100

    update twitter contract

--- Exercises/Twitter.sol
+++ Exercises/Twitter.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.8.18;
 
 contract Twitter {
 
-    mapping(address => string) public tweets;
+    mapping(address => string[]) public tweets;
 
     function addTweet(string memory _tweet) public {
-        tweets[msg.sender] = _tweet;
+        tweets[msg.sender].push(_tweet);
     }
 
-    function getTweet(address _owner) public view returns(string memory) {
+    function getTweets(address _owner) public view returns(string[] memory) {
         return tweets[_owner];
+    }
+
+    function getTweet(address _owner, uint _index) public view returns(string memory) {
+        return tweets[_owner][_index];
     }
 }
