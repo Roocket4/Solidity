b2e717a6969e3c91915c4231fa3500e0f62572a0
zvonimir.mesaric <zvonimir.mesaric@bozic.io>
2023-11-22 22:52:04 +0100

    update twitter contract

--- Exercises/Twitter.sol
+++ Exercises/Twitter.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.18;
 
 contract Twitter {
 
+    uint16 maxTweetLength;
+    address private owner;
+
     struct Tweet {
         address author;
         string content;
@@ -12,7 +15,15 @@ contract Twitter {
 
     mapping(address => Tweet[]) public tweets;
 
+    constructor() {
+        owner = msg.sender;
+        maxTweetLength = 280;
+    }
+
     function addTweet(string memory _tweet) public {
+
+        require(bytes(_tweet).length <= maxTweetLength);
+
         Tweet memory newTweet = Tweet({
             author: msg.sender,
             content: _tweet,
@@ -29,5 +40,14 @@ contract Twitter {
 
     function getTweets(address _owner) public view returns(Tweet[] memory) {
         return tweets[_owner];
+    }
+
+    function changeMaxTweetLength(uint16 length) public onlyOwner {
+        maxTweetLength = length;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the contract owner!");
+        _;
     }
 }
