8c07fc6489dd01d8034129af19cb5e31ac67c96c
zvonimir.mesaric <zvonimir.mesaric@bozic.io>
2023-11-22 15:39:26 +0100

    update calculator to fancy calculator

--- Exercises/Calculator.sol
+++ Exercises/Calculator.sol
@@ -6,23 +6,29 @@ contract Calculator {
 
     uint256 public result = 0;
 
-    function add(uint256 value) public {
-       result += value;
+    function add(uint256 a, uint256 b) internal {
+       result = a + b;
     }
 
-    function subtract(uint256 value) public {
-        result -= value;
+    function subtract(uint256 a, uint256 b) internal {
+        result = a - b;
     }
 
-    function multiply(uint256 value) public {
-        result *= value;
+    function multiply(uint256 a, uint256 b) internal {
+        result = a * b;
     }
 
-    function divide(uint256 value) public {
-        result /= value;
+    function divide(uint256 a, uint256 b) internal {
+        result = a / b;
     }
+}
 
-    function powerOf(uint256 value) public {
-        result %= value;
+contract computeCalculation is Calculator {
+    function calculate(uint256 a, uint256 b, string memory operation) public {
+        if(keccak256(abi.encodePacked(operation)) == keccak256(abi.encodePacked("add"))) add(a, b);
+        else if(keccak256(abi.encodePacked(operation)) == keccak256(abi.encodePacked("sub"))) subtract(a, b);
+        else if(keccak256(abi.encodePacked(operation)) == keccak256(abi.encodePacked("mult"))) multiply(a, b);
+        else if(keccak256(abi.encodePacked(operation)) == keccak256(abi.encodePacked("div"))) divide(a, b);
+        else revert("Invalid Operation Type");
     }
 }
