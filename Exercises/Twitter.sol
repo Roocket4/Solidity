281f39b5effcb79329ef159ae81018fbe93525d7
zvonimir.mesaric <zvonimir.mesaric@bozic.io>
2023-11-23 15:36:52 +0100

    finish up tweeter contract, update code

--- Exercises/Twitter.sol
+++ Exercises/Twitter.sol
@@ -11,6 +11,7 @@ contract Twitter {
         string content;
         uint256 timestamp;
         uint256 likes;
+        uint256 id;
     }
 
     mapping(address => Tweet[]) public tweets;
@@ -20,6 +21,10 @@ contract Twitter {
         maxTweetLength = 280;
     }
 
+    event TweetCreated(uint256 id, address author, string content, uint256 timestamp);
+    event TweetLiked(uint256 id, address author, address likeFrom, uint256 currentLikeCount);
+    event TweetDisliked(uint256 id, address author, address likeFrom, uint256 currentLikeCount);
+
     function addTweet(string memory _tweet) public {
 
         require(bytes(_tweet).length <= maxTweetLength);
@@ -28,10 +33,12 @@ contract Twitter {
             author: msg.sender,
             content: _tweet,
             timestamp: block.timestamp,
-            likes: 0
+            likes: 0,
+            id: tweets[msg.sender].length
         });
 
         tweets[msg.sender].push(newTweet);
+        emit TweetCreated(newTweet.id, newTweet.author, newTweet.content, newTweet.timestamp);
     }
 
     function getTweet(address _owner, uint _index) public view returns(Tweet memory) {
@@ -44,6 +51,21 @@ contract Twitter {
 
     function changeMaxTweetLength(uint16 length) public onlyOwner {
         maxTweetLength = length;
+    }
+
+    function likeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "The tweet you are refering to does not exist");
+
+        tweets[author][id].likes++;
+        emit TweetLiked(id, author, msg.sender, tweets[author][id].likes);
+    }
+
+    function dislikeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id, "The tweet you are refering to does not exist");
+        require(tweets[author][id].likes > 0, "Tweet has no likes");
+
+        tweets[author][id].likes--;
+        emit TweetDisliked(id, author, msg.sender, tweets[author][id].likes);
     }
 
     modifier onlyOwner() {
